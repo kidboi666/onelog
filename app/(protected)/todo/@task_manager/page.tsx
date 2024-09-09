0b1c9675e408a48d9e60197e1cef4d9f44232041
kidboi666 <kidboi666@gmail.com>
2024-09-09 20:48:55 +0900

    ✨fix : 빌드 에러 수정

--- app/(protected)/todo/@task_manager/page.tsx
+++ app/(protected)/todo/@task_manager/page.tsx
@@ -13,65 +13,9 @@ import Title from '@/components/shared/Title'
 import { useInput } from '@/hooks/useInput'
 import useStateChange from '@/hooks/useStateChange'
 import cn from '@/lib/cn'
-import { FormEvent, RefObject, useCallback, useEffect, useState } from 'react'
+import { FormEvent, useCallback, useEffect, useState } from 'react'
 
-interface TodoProps {
-  todo: string
-  onDelete?: (selectedTodo: string) => void
-  onSuccess?: (selectedTodo: string) => void
-}
-
-const Todo = ({ todo, onDelete, onSuccess }: TodoProps) => {
-  return (
-    <List.Row>
-      <Text
-        className={cn(
-          'flex animate-fade-in items-center justify-between gap-4 text-xs',
-          onDelete && onSuccess ? '' : 'text-slate-400',
-        )}
-      >
-        {todo}
-        {onDelete && onSuccess ? (
-          <Box className="flex gap-2">
-            <Button
-              size="emptyStyle"
-              className="size-6 rounded-full"
-              onClick={() => onSuccess!(todo)}
-            >
-              <Icon size={20} view={20}>
-                <path
-                  fillRule="evenodd"
-                  d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                  clipRule="evenodd"
-                />
-              </Icon>
-            </Button>
-            <Button
-              size="emptyStyle"
-              variant="secondary"
-              onClick={() => onDelete!(todo)}
-              className="size-6 rounded-full text-gray-400"
-            >
-              <Icon size={20} view={27}>
-                <path
-                  d="M8.31361 17.9346C7.94447 18.3037 7.92689 18.9629 8.3224 19.3408C8.70033 19.7363 9.3683 19.7188 9.73744 19.3496L14.0001 15.0869L18.2628 19.3496C18.6408 19.7275 19.2911 19.7363 19.6691 19.3408C20.0646 18.9629 20.0558 18.3037 19.6779 17.9258L15.4152 13.6631L19.6779 9.40918C20.0558 9.02246 20.0646 8.37207 19.6691 7.99414C19.2911 7.59863 18.6408 7.60742 18.2628 7.98535L14.0001 12.248L9.73744 7.98535C9.3683 7.61621 8.70033 7.59863 8.3224 7.99414C7.92689 8.37207 7.94447 9.03125 8.31361 9.40039L12.5763 13.6631L8.31361 17.9346Z"
-                  fill="currentColor"
-                ></path>
-              </Icon>
-            </Button>
-          </Box>
-        ) : null}
-      </Text>
-    </List.Row>
-  )
-}
-
-interface Props {
-  className?: string
-  statusRef: RefObject<HTMLDivElement>
-}
-
-export default function TodoPage({ className, statusRef }: Props) {
+export default function TodoPage() {
   const [todoText, onChangeTodoText, setTodoText] = useInput('')
   const [todos, setTodos] = useState<string[]>([])
   const [successTodos, setSuccessTodos] = useState<string[]>([])
@@ -123,11 +67,8 @@ export default function TodoPage({ className, statusRef }: Props) {
 
   return (
     <Container
-      ref={statusRef}
-      dataStatus="closed"
       className={cn(
         'right-0 top-full w-96 cursor-default max-md:fixed max-md:left-1/2 max-md:w-[calc(100dvw-40px)] max-md:-translate-x-1/2',
-        className,
       )}
     >
       <FormContainer onSubmit={handleTodoChange} className="w-full">
@@ -170,5 +111,56 @@ export default function TodoPage({ className, statusRef }: Props) {
         </Box>
       </FormContainer>
     </Container>
+  )
+}
+
+interface TodoProps {
+  todo: string
+  onDelete?: (selectedTodo: string) => void
+  onSuccess?: (selectedTodo: string) => void
+}
+
+function Todo({ todo, onDelete, onSuccess }: TodoProps) {
+  return (
+    <List.Row>
+      <Text
+        className={cn(
+          'flex animate-fade-in items-center justify-between gap-4 text-xs',
+          onDelete && onSuccess ? '' : 'text-slate-400',
+        )}
+      >
+        {todo}
+        {onDelete && onSuccess ? (
+          <Box className="flex gap-2">
+            <Button
+              size="emptyStyle"
+              className="size-6 rounded-full"
+              onClick={() => onSuccess!(todo)}
+            >
+              <Icon size={20} view={20}>
+                <path
+                  fillRule="evenodd"
+                  d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                  clipRule="evenodd"
+                />
+              </Icon>
+            </Button>
+            <Button
+              size="emptyStyle"
+              variant="secondary"
+              onClick={() => onDelete!(todo)}
+              className="size-6 rounded-full text-gray-400"
+            >
+              <Icon size={20} view={27}>
+                <path
+                  d="M8.31361 17.9346C7.94447 18.3037 7.92689 18.9629 8.3224 19.3408C8.70033 19.7363 9.3683 19.7188 9.73744 19.3496L14.0001 15.0869L18.2628 19.3496C18.6408 19.7275 19.2911 19.7363 19.6691 19.3408C20.0646 18.9629 20.0558 18.3037 19.6779 17.9258L15.4152 13.6631L19.6779 9.40918C20.0558 9.02246 20.0646 8.37207 19.6691 7.99414C19.2911 7.59863 18.6408 7.60742 18.2628 7.98535L14.0001 12.248L9.73744 7.98535C9.3683 7.61621 8.70033 7.59863 8.3224 7.99414C7.92689 8.37207 7.94447 9.03125 8.31361 9.40039L12.5763 13.6631L8.31361 17.9346Z"
+                  fill="currentColor"
+                ></path>
+              </Icon>
+            </Button>
+          </Box>
+        ) : null}
+      </Text>
+    </List.Row>
   )
 }
