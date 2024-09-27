46920df904676808cf788c38bf9994bbd90e006e
kidboi666 <kidboi666@gmail.com>
2024-09-27 13:43:54 +0900

    🛠️fix : 빌드 에러 수정

--- app/(protected)/@modal/(.)delete_todo/[todoId]/page.tsx
+++ app/(protected)/@modal/(.)delete_todo/[todoId]/page.tsx
@@ -1,5 +1,10 @@
 import DeleteTodoModal from '@/app/(protected)/(modals)/delete_todo/[todoId]/page'
 
-export default function Page() {
-  return <DeleteTodoModal />
+interface Props {
+  params: { todoId: string }
+  searchParams: { folder_id: string }
+}
+
+export default function Page({ params, searchParams }: Props) {
+  return <DeleteTodoModal params={params} searchParams={searchParams} />
 }
