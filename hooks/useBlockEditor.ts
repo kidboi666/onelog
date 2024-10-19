c1efc18a73c6f979da84fbb2ed333741d48997c7
kidboi666 <kidboi666@gmail.com>
2024-10-19 11:48:21 +0900

    🛠️fix : 일단 highlight 삭제

--- hooks/useBlockEditor.ts
+++ hooks/useBlockEditor.ts
@@ -2,7 +2,6 @@
 
 import CharacterCount from '@tiptap/extension-character-count'
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
-import Highlight from '@tiptap/extension-highlight'
 import Placeholder from '@tiptap/extension-placeholder'
 import { useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
@@ -37,7 +36,6 @@ export default function useBlockEditor({
       CodeBlockLowlight.configure({
         lowlight,
       }),
-      Highlight.configure({ multicolor: true }),
       Placeholder.configure({
         placeholder,
         showOnlyCurrent: false,
