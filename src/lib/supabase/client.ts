c5ce242fa8ae3dbcfd807baf8a28d50c09f94eb1
예비 리액트맨 <70879978+kidboi666@users.noreply.github.com>
2024-12-27 17:13:46 +0900

    ♻️refactor : supabase client 생성 로직 수정

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,6 +1,19 @@
 import { createBrowserClient } from '@supabase/ssr'
+import { SupabaseClient } from '@supabase/supabase-js'
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-)
+let client: SupabaseClient | undefined
+
+const getSupabaseClient = () => {
+  if (client) {
+    return client
+  }
+
+  client = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  )
+
+  return client
+}
+
+export const supabase = getSupabaseClient()
