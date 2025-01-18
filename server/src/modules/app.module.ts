4b1afe33677ec7eacf24071bb0d5fbc25b3c5fd2
예비 리액트맨 <70879978+kidboi666@users.noreply.github.com>
2025-01-18 20:10:04 +0900

    🛠️fix : db synchronice 환경에 따른 로직 추가

--- server/src/modules/app.module.ts
+++ server/src/modules/app.module.ts
@@ -25,9 +25,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      database:
+        process.env.NODE_ENV === 'test'
+          ? process.env.TEST_DB_NAME
+          : process.env.DB_NAME,
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'production',
     }),
     CommentsModule,
     FollowsModule,
