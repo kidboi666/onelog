63030dae2aa2e33d5ae288dd80cdb2f809bc7346
예비 리액트맨 <70879978+kidboi666@users.noreply.github.com>
2025-01-19 17:04:57 +0900

    ✨feat : 환경 변수 nest 구조에 맞게 추가 및 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,23 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-import { ValidationPipe } from '@nestjs/common';
-
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: [process.env.COOKIE_SECRET!],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
-  await app.listen(process.env.PORT!);
-  console.log(`Server running at http://localhost:${process.env.PORT!} `);
+  await app.listen(process.env.PORT!, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT!} `);
+  });
 }
 
 void bootstrap();
--- server/src/modules/app.module.ts
+++ server/src/modules/app.module.ts
@@ -1,4 +1,7 @@
-import { Module } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts.module';
 import { CommentsModule } from './comments.module';
 import { FollowsModule } from './follows.module';
@@ -11,26 +14,32 @@ import { TodoModule } from './todo.module';
 import { UsedWordsModule } from './used-words.module';
 import { UsersModule } from './users.module';
 import { WordDictionariesModule } from './word-dictionaries.module';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from '../services/app.service';
+import { AppController } from '../controllers/app.controller';
+
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database:
-        process.env.NODE_ENV === 'test'
-          ? process.env.TEST_DB_NAME
-          : process.env.DB_NAME,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: process.env.NODE_ENV !== 'production',
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          database: config.get<string>('DB_NAME'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: config.get<string>('NODE_ENV') !== 'production',
+          dropSchema: config.get<string>('NODE_ENV') === 'test',
+        };
+      },
     }),
     CommentsModule,
     FollowsModule,
@@ -45,5 +54,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     UsersModule,
     WordDictionariesModule,
   ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {}
+
+  // 애플리케이션이 들어오는 트래픽을 수신할 때 자동으로 호출 됨
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: [this.configService.get<string>('COOKIE_SECRET')],
+        }),
+      )
+      .forRoutes('*');
+    // 전체 애플리케이션에 들어오는 모든 요청에 이 미들웨어를 사용하겠다는 와일드카드
+  }
+}
