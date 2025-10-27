462bfc1e54c87bb2ad0b98732672e842d34e4c71
kidboi666 <kidboi666@gmail.com>
2025-10-27 12:20:20 +0900

    feat: Sidebar 및 관련 UI 컴포넌트 추가
    - 추가: Sidebar 컴포넌트 구현 (확장, 축소, 모바일 대응 등 포함)
    - 추가: Sidebar 관련 컴포넌트 구현 (`Header`, `Footer`, `Menu`, `MenuItem`, `Separator`, `Input` 등)
    - 추가: `useIsMobile` 훅 생성으로 모바일 상태 관리
    - 추가: Radix UI의 Separator, Sheet, Tooltip을 활용한 커스텀 컴포넌트 구현
    - 추가: 전역 폰트(`goorm-sans`) 설정 및 Tailwind 스타일 업데이트
    - 추가: `@radix-ui/react-separator` 종속성 설치 및 관련 설정
    - 수정: 기존 `cn` 유틸리티 경로를 `"@/shared/lib/utils"`로 수정
    - 개선: Dialog, Button, Tooltip 등 기존 UI 컴포넌트의 코드 재정렬 및 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import ReactQueryProvider from "@/core/providers/ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class">{children}</ThemeProvider>
+        <ThemeProvider attribute="class">
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
