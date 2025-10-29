44990226f221b7e61e3a63a2c95561ca08da6aab
kidboi666 <kidboi666@gmail.com>
2025-10-29 14:06:28 +0900

    refactor: TODO 기능 제거
    - TODO 관련 모든 컴포넌트 및 페이지 제거
    - TODO 병렬 경로 제거 (@main, @sidebar, @todo_info)
    - TODO 레이아웃 및 라우팅 제거
    - 기능은 새로운 아키텍처로 재구현될 예정

--- src/app/(playground)/_todo/@sidebar/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Spinner from "@/src/components/Spinner";
-
-export default function Loading() {
-  return (
-    <Spinner.Container>
-      <Spinner size={60} />
-    </Spinner.Container>
-  );
-}
--- src/app/(playground)/_todo/route.ts
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export async function GET(_: Request) {
-  redirect(`/todo/main`)
-}
