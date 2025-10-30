107278debbf52464b67bab5002c13c0a769300cf
kidboi666 <kidboi666@gmail.com>
2025-10-30 23:04:21 +0900

    second commit

---
+++ src/db/index.ts
@@ -0,0 +1,5 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+const sql = postgres(process.env.DATABASE_URL!, { prepare: false });
+export const db = drizzle(sql);
---
+++ src/db/schema.ts
@@ -0,0 +1,29 @@
+import {
+  boolean,
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { authUsers } from "drizzle-orm/supabase";
+
+export const userInfo = pgTable("user_info", {
+  id: uuid()
+    .primaryKey()
+    .references(() => authUsers.id), // Supabase Auth 연동
+  user_name: text().notNull(),
+  avatar_url: text(),
+  about_me: text(),
+  created_at: timestamp().defaultNow().notNull(),
+});
+
+export const posts = pgTable("posts", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  user_id: uuid().references(() => userInfo.id),
+  content: text().notNull(),
+  emotion_level: integer().notNull(),
+  is_public: boolean().default(true),
+  created_at: timestamp().defaultNow().notNull(),
+  updated_at: timestamp().defaultNow().notNull(),
+});
